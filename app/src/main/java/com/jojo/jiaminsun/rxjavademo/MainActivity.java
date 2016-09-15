e29b03f374f1ddec64ddd636ccb6e7733adb80b2
823282324@qq.com <19910902sjm>
2016-09-15 19:36:37 +0800

    change

--- app/src/main/java/com/jojo/jiaminsun/rxjavademo/MainActivity.java
+++ app/src/main/java/com/jojo/jiaminsun/rxjavademo/MainActivity.java
@@ -8,45 +8,116 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    Button btn_click;
+    LinearLayout line_image;
+    private MyHoveringScrollView view_hover;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+       // setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+//        btn_click = (Button)findViewById(R.id.btn_click);
+//        line_image = (LinearLayout)findViewById(R.id.line_image);
+//        btn_click.setOnClickListener(this);
+        view_hover = (MyHoveringScrollView) findViewById(R.id.view_hover);
+        view_hover.setTopView(R.id.top);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                  Toast.makeText(MainActivity.this,"点击了按钮", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    private boolean isLayoutIn = true;
+    private boolean isAnimationRuning;
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onClick(View v) {
+        switch (v.getId()){
+//            case R.id.btn_click:
+//                switchOpte();
+//                break;
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void switchOpte(){
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+        Animation topAnimation;
+        Animation bottomAnimation;
+        if (isLayoutIn) { // 划出动画 消失 往左边移动
+
+            bottomAnimation = new AlphaAnimation(1, 0);
+            topAnimation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0, Animation.RELATIVE_TO_SELF, -1,
+                    Animation.RELATIVE_TO_SELF,0, Animation.RELATIVE_TO_SELF, 0);
+            topAnimation.setAnimationListener(new Animation.AnimationListener() {
+
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    isAnimationRuning = true;
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+
+                    isAnimationRuning = false;
+                    line_image.setVisibility(View.GONE);
+
+
+
+                }
+            });
+        } else { // 划入动画 显示 往右边移动
+
+            line_image.setVisibility(View.VISIBLE);
+
+            bottomAnimation = new AlphaAnimation(0, 1);
+            topAnimation = new TranslateAnimation(Animation.RELATIVE_TO_SELF, -1, Animation.RELATIVE_TO_SELF, 0,
+                    Animation.RELATIVE_TO_SELF, 0, Animation.RELATIVE_TO_SELF, 0);
+            topAnimation.setAnimationListener(new Animation.AnimationListener() {
+
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    isAnimationRuning = true;
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+
+                    isAnimationRuning = false;
+
+                }
+            });
         }
 
-        return super.onOptionsItemSelected(item);
+        topAnimation.setFillAfter(true);
+
+        bottomAnimation.setFillAfter(true);
+        topAnimation.setDuration(300);
+        bottomAnimation.setDuration(300);
+        isLayoutIn = !isLayoutIn;
+        line_image.startAnimation(topAnimation);
+
     }
 }
